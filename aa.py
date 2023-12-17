1d77d632821b7aa8420ae22bb127578555b73ceb
Çağıl Emek Kurtul <104488106+cagilemek@users.noreply.github.com>
2023-12-17 18:45:05 +0300

    Add files via upload

---
+++ aa.py
@@ -0,0 +1,12 @@
+"Klavyeden girilen sayının pozitif mi negatif mi yoksa sıfır mı olduğunu bulan python kodunu yazınız."
+a = int(input("sayı gir: "))
+
+if (a < 0):
+    print ("sayı negatif.")
+
+if (a == 0): 
+    print("sayı sıfır.")
+
+if (a > 0):
+    print("sayı pozitif")
+
